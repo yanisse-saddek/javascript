7fcf3612c1fb33ed11084753c490645493b493a9
yanisse-saddek <yanisse321@gmail.com>
2022-02-04 11:16:44 +0100

    update

--- jour-7/pendu.js
+++ jour-7/pendu.js
@@ -1,55 +1,53 @@
-function game(){
-  var mysteryWord = "ordinateur";
-mysteryWordArray = mysteryWord.split("");
-letter = [];
-count = 10;
-hiddenWord = mysteryWordArray.map(function (letter) {
-  return letter;
-});
+function game() {
 
-for (i = 0; i < mysteryWord.length; i++) {
-  hiddenWord[i] = "-";
-}
-
-function check(promptLetter) {
-  letter.push(lettre);
+  var mysteryWord = "tablette";
+  mysteryWordArray = mysteryWord.split("");
+  letter = [];
+  count = 10;
+  hiddenWord = mysteryWordArray.map(function (letter) {
+    return letter;
+  });
   for (i = 0; i < mysteryWord.length; i++) {
-    for (j = 0; j < letter.length; j++) {
-      if (mysteryWord[i] === letter[j]) {
-        hiddenWord[i] = letter[j];
+    hiddenWord[i] = "-";
+  }
+  function check(promptLetter) {
+
+    letter.push(lettre);
+    for (i = 0; i < mysteryWord.length; i++) {
+      for (j = 0; j < letter.length; j++) {
+        if (mysteryWord[i] === letter[j]) {
+          hiddenWord[i] = letter[j];
+        }
       }
     }
-  }
 
-  for(i=0; i<lettre.length; i++){
-    console.log(lettre[i])
-    if(hiddenWord.includes(lettre[i])){
-    }else{
-      count--
+    for (i = 0; i < lettre.length; i++) {
+      if (!hiddenWord.includes(lettre[i])) {
+        count--;
+      }
+    }
+    console.clear()
+
+    if (hiddenWord.join("") === mysteryWord) {
+      console.log("Bravo tu as réussi ! Le mot était:", mysteryWord);
+    } else if (count === 0) {
+      console.log("Tu as perdu ! Le mot était:", mysteryWord);
+    } else if (count <= 10) {
+      console.log("Il te reste", count.toString(), "essais!");
+      promptText();
     }
   }
-  if (hiddenWord.join("") === mysteryWord) {
-    console.log('Bravo tu as réussi ! Le mot était:', mysteryWord);
-  }else if(count===0){
-    console.log('Tu as perdu ! Le mot était:', mysteryWord)
-} else if(count<=10){
-    console.log('Il te reste', count.toString(), 'essais!')
-    console.log(hiddenWord.join(""));
-    promptText();
+
+  function promptText() {
+    var prompt = require("prompt");
+    console.log("Le mot contient", hiddenWord.length, "caractères :", hiddenWord.join(''))
+    prompt.start();
+    prompt.get("Lettre", function (err, result) {
+      lettre = result.Lettre;
+      check(lettre);
+    });
   }
+  promptText();
 }
 
-
-function promptText() {
-  var prompt = require("prompt");
-  prompt.start();
-  prompt.get("Lettre", function (err, result) {
-
-    lettre = result.Lettre;
-    check(lettre);
-  });
-}
-promptText();
-}
-
-game()
+game();
