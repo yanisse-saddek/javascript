62c2d1722c6ee43875cfc62abebbab6cfb17e763
yanisse-saddek <yanisse321@gmail.com>
2022-02-07 17:17:52 +0100

    update

--- jour-8/jour08.js
+++ jour-8/jour08.js
@@ -1,35 +1,35 @@
 // // Countries
 
+const request = require('request');
+const { json } = require('stream/consumers');
+var countriesNames = []
+var apiGet = "https://restcountries.com/v3.1/all";
+
+request(apiGet, function(err, response, body){
+    var countries = JSON.parse(body)
+    for(i=0; i<countries.length; i++){
+        countriesNames.push(countries[i].name.common)
+    }
+    countriesNames.join('')
+    // console.log(countriesNames)
+})
+
+// Chuck Norris
+
 // const request = require('request');
-// const { json } = require('stream/consumers');
-// var countriesNames = []
-// var apiGet = "https://restcountries.com/v3.1/all";
-
-// request(apiGet, function(err, response, body){
-//     var countries = JSON.parse(body)
-//     for(i=0; i<countries.length; i++){
-//         countriesNames.push(countries[i].name.common)
-//     }
-//     countriesNames.join('')
-//     console.log(countriesNames)
-// })
-
-// // Chuck Norris
-
-// const request = require('request');
-// var apiGet = "https://api.chucknorris.io/jokes/random";
-// function getFact(){
-//     request(apiGet, function(err, response, body){
-//         var joke = JSON.parse(body)
-//         console.log(joke.value)
+var apiGet = "https://api.chucknorris.io/jokes/random";
+function getFact(){
+    request(apiGet, function(err, response, body){
+        var joke = JSON.parse(body)
+        console.log(joke.value)
     
-//     })
-// }
+    })
+}
 // getFact()
 
 // // Pokemon
 
-const request = require('request');
+// const request = require('request');
 
 function randomPokemon(number){
     var apiGet = "https://pokeapi.co/api/v2/pokemon/";
@@ -40,5 +40,5 @@ function randomPokemon(number){
         })
 
 }
-randomPokemon(5)
+// randomPokemon(5)
 
